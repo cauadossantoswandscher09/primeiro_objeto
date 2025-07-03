2e180858e67083e96afb2b3416b19917adf00c8b
cauadossantoswandscher09 <caua.wandscher@escola.pr.gov.br>
2025-07-03 08:39:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+function chama_aluno(){
+
+    let aluno = {
+        nome: 'Darlan Souza',
+        idade: 23,
+        altura: 1.93,
+        nota: 5.10
+
+    };
+    alert(aluno.nome);
+
+    document.getElementById('nome_aluno').textContent = 'nome: ' + aluno.nome;
+    document.getElementById('idade_aluno').textContent = 'idade' + aluno.idade;
+    document.getElementById('altura_aluno').textContent = 'altura' + aluno.altura;
+    document.getElementById('nota_aluno').textContent = 'nota' + aluno.nota;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
